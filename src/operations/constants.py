228467eef56d8bd633dd532c704cdbf0a196bb8d
manaskhandeshe-delphix <manas.khandeshe@delphix.com>
2023-03-14 16:04:33 +0530

    CE-183 "show dbs" command does not work with --eval for the mongo client.
    CE-186 Intermittent: MongoDB errors out saying Address already in use if temporary socket files for available ports exists on the staging.

--- src/operations/constants.py
+++ src/operations/constants.py
@@ -9,6 +9,8 @@ class Globals:
     RS_INITIATE = "{mongo_shell_path} --port {port} --quiet --eval " \
                   "'rs.initiate()'"
 
+    # SOCKET PATH
+    MONGODB_SOCKET_FILE_PATH = "/tmp/mongodb-{port}.sock"
 
 
     EXPECTED_RS_INITIATE_ERROR = "MongoServerError: already initialized"
